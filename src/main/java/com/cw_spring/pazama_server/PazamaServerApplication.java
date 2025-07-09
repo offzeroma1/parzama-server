8c6ac6684aefe88cf098cfeba5cc9e76ab7b0ae3
offzeroma1 <offzeroma1@gmail.com>
2025-07-09 17:06:46 +0900

    init : pazama-server start

---
+++ src/main/java/com/cw_spring/pazama_server/PazamaServerApplication.java
@@ -0,0 +1,13 @@
+package com.cw_spring.pazama_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PazamaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PazamaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cw_spring/pazama_server/PazamaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cw_spring.pazama_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PazamaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
